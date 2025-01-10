632bc88326d03982c24904c478c601f3600ae0ad
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-01-10 12:29:08 -0300

    added some stuff for day 30 and 31

--- Day 30/PASS/main.py
+++ Day 30/PASS/main.py
@@ -5,6 +5,28 @@ import pyperclip
 import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+#Search Website
+
+def search_website():
+    website = website_entry.get()
+    try: 
+            with open("Day 30\PASS\data.json", "r") as data_file:
+                data = json.load(data_file)  
+    except FileNotFoundError:
+        messagebox.showinfo(title="File not Found", message="File does not exist. Maybe try creating something?")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website}", message=f"Email: {email} \nPassword: {password}")          
+        else:
+            messagebox.showinfo(title="Website not Found", message="Website does not exist in the system.")
+
+
+
+
+
 #Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -39,18 +61,18 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("Day 30\PASS\data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent=4)
-            
-            """ data = json.load(data_file)
-            print(data) """
-            
-            data = json.load(data_file)
+        try: 
+            with open("Day 30\PASS\data.json", "r") as data_file:
+                data = json.load(data_file)     
+        except FileNotFoundError:
+            with open("Day 30\PASS\data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             data.update(new_data)
             
             with open("Day 30\PASS\data.json", "w") as data_file:
-                json.dump(new_data, data_file, indent=4)
-            
+                json.dump(data, data_file, indent=4)
+        finally:  
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
@@ -75,19 +97,21 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=17)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
-password_entry = Entry(width=21)
+email_entry.insert(0, "example@gmail.com")
+password_entry = Entry(width=17)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_website_button = Button(text="Search", width=17, command=search_website)
+search_website_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
